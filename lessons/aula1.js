1010cae516e407e5ea763b22cfb22550abd66d2d
camilagomo <37373931+camilagomo@users.noreply.github.com>
2025-06-11 12:08:25 -0300

    Update aula1.js

--- lessons/aula1.js
+++ lessons/aula1.js
@@ -1,1 +1,12 @@
+// 10-06-2025
+//Informe quantos dólares você tem
+const quantidadeDeDolares = 38.0;
 
+//Pergunte a um adulto quantos Reais vale um Dólar
+const valorReal = 5.57;
+
+//Multiplique a quantidade de dólares que você tem pelo valor do Real
+const resultadoEmReais = quantidadeDeDolares * valorReal;
+
+//Escreva o resultado da sua conta
+console.log(resultadoEmReais);
