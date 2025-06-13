bf0c32da6a124d334930d579762ec6f0c1c00210
camilagomo <37373931+camilagomo@users.noreply.github.com>
2025-06-12 22:25:10 -0300

    Delete lessons/aula2_functions.js

--- lessons/aula2_functions.js
+++
@@ -1,6 +0,0 @@
-function calculaDesconto(valorDoDesconto, valorDoProduto) {
-  return valorDoProduto - valorDoDesconto;
-}
-
-const valorFinalDoProduto = calculaDesconto(10.0, 55.0);
-console.log(valorFinalDoProduto);
